43958ef5bc0c0831d042ffafab973554ac82d2e8
Durgeshchaudhary001 <dk1570801@gmail.com>
2025-09-04 20:59:05 +0530

    Add push

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -9,7 +9,9 @@ export const AppContext=createContext();
 const AppContextProvider = (props)=>{
 
     const currencySymbol = '$'
-    const backendUrl = 'http://localhost:4000'
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+
     const [doctors,setDoctors] = useState([])
 
     const [token,setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') :false )
